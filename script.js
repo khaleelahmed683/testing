71de7c4da4a4eef9f415e34ae7171a2288fc04fa
Syed Khaleel Ahmed <khaleelwork1@gmail.com>
2024-03-18 15:25:56 +0530

    JS File added with Event Listener

---
+++ script.js
@@ -0,0 +1,12 @@
+var h1 = document.querySelector('#nav h1');
+
+h1.addEventListener('mouseenter',function(){
+
+    
+    document.querySelector('#nav h1').style.backgroundColor = "red";
+});
+
+h1.addEventListener('mouseleave',function(){
+    
+    document.querySelector('#nav h1').style.backgroundColor = "black";
+}) 
